6d59ee9cd7d7d2114ba055f04d4753a601807dff
rakeemshelby-macbook <rokshelby@gmail.com>
2025-07-19 18:14:05 -0500

    needed to add ip

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));      // Serve static files (HTML, JS)
+app.use(express.json());                // Parse JSON in POST requests
+app.use(express.urlencoded({ extended: true })); // Parse form data
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
