93f8c6e40d7b17a047983b9a9b395fe807b8cb46
rakeemshelby-macbook <rokshelby@gmail.com>
2025-07-19 19:09:40 -0500

    vmuse use.

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const PORT = 3000;
      // Serve static files (HTML, JS)
 app.use(express.json());                // Parse JSON in POST requests
 app.use(express.urlencoded({ extended: true })); // Parse form data
-app.use(express.static(path.join(__dirname,'../vMuse')));
+app.use(express.static(path.join(__dirname)));
+
+app.use('/vMuse', express.static(path.join(__dirname,'../vMuse')));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
